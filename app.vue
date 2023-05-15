ee665bb1bb8bcf71d02eb808104989152b2ee3d9
akbarchives <rizqiakbar2412@gmail.com>
2023-05-15 10:46:57 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <nav
+      class="px-8 py-4 pl-16 w-100 border-b fixed bg-[#fde68a] border-black w-full flex flex-row justify-between items-end"
+    >
+      <h1 class="text-slate-900 font-semibold text-3xl">Atomic Habits</h1>
+    </nav>
+    <!-- section 1 -->
+    <section class="h-screen p-16 flex flex-col justify-center items-center">
+      <header
+        class="h-100 w-3/4 flex sm:flex-col md:flex-row justify-center items-center gap-10"
+      >
+        <img
+          class="sm:mt-5 sm:w-40 md:w-64 shadow-xl bg-slate-500"
+          src="https://m.media-amazon.com/images/I/91bYsX41DVL._AC_UF894,1000_QL80_.jpg"
+          alt=""
+        />
+        <h2 class="font-serif italic text-lg">
+          "Changes that seem small and unimportant at first will compound into
+          remarkable results if you're willing to stick with them for years."
+          <br />
+          - James Clear
+        </h2>
+      </header>
+    </section>
+    <!-- section tips -->
+    <section
+      class="w-full p-16 bg-[#fde68a] font-serif flex flex-col justify-center items-center"
+    >
+      <h1 class="text-2xl font-bold mb-8 mt-8">
+        Tips dari buku <span class="underline">Atomic Habits</span>
+      </h1>
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?focus"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">1. Fokus</span> <br />
+          Fokuslah pada perubahan kecil seiring waktu: Alih-alih mencoba untuk
+          melakukan perubahan besar dan drastis dalam hidup Anda, fokuslah pada
+          kebiasaan kecil yang dapat Anda lakukan secara konsisten setiap hari.
+        </h2>
+      </div>
+
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row-reverse justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?habit"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">2. Kebiasaan</span> <br />
+          Buatlah kebiasaan menjadi bagian dari identitas Anda: Alih-alih
+          melihat kebiasaan sebagai tindakan yang harus dilakukan, lihatlah
+          mereka sebagai bagian dari siapa Anda.
+        </h2>
+      </div>
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?gift"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">3. Reward</span> <br />
+          Gunakan sistem penghargaan dan hukuman: Sistem penghargaan dan hukuman
+          dapat membantu memotivasi Anda untuk tetap konsisten dengan kebiasaan
+          baru.
+        </h2>
+      </div>
+
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row-reverse justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?friend"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">4. Mentor</span> <br />
+          Temukan teman atau mentor yang dapat membantu: Menemukan teman atau
+          mentor yang memiliki tujuan serupa dengan Anda dapat membantu
+          memotivasi dan mempertahankan komitmen
+        </h2>
+      </div>
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?easy"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">5. Buatlah Mudah</span>
+          <br />
+          Buatlah kebiasaan mudah dilakukan: Kebiasaan yang mudah dilakukan
+          cenderung lebih berhasil daripada kebiasaan yang sulit dilakukan.
+        </h2>
+      </div>
+
+      <div
+        class="sm:w-full md:w-full lg:w-2/3 lg:mx-64 rounded-lg overflow-hidden mb-6 bg-white flex sm:flex-col md:flex-row-reverse justify-center items-center shadow-lg"
+      >
+        <img
+          class="sm:w-full sm:h-64 sm:object-cover sm:object-center"
+          src="https://source.unsplash.com/400x400?wrong"
+          alt=""
+        />
+        <h2 class="p-4 ml-8">
+          <span class="text-xl font-semibold py-4">6. Teruskan</span> <br />
+          Teruskan meskipun terjadi kesalahan: Kesalahan adalah bagian alami
+          dari proses pembentukan kebiasaan baru. Jangan biarkan kesalahan
+          membuat Anda menyerah atau merasa buruk tentang diri sendiri.
+        </h2>
+      </div>
+    </section>
+    <footer class="p-6 text-lg text-center">
+      <h2>summaries by 🤖 chatgpt</h2>
+    </footer>
+  </div>
+</template>
---
+++ components/Tbutton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="btn btn-blue p-5 rounded-lg bg-blue-500 font-bold">
+    <slot>Button</slot>
+  </button>
+</template>
